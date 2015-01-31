3d48128c0eec065b704adbf4860634abf4f194e3
Kenneth Hernandez <kennysmoothx@gmail.com>
2015-01-31 18:40:15 -0500

    Disable collect

--- FiftyOne/wsgi.py
+++ FiftyOne/wsgi.py
@@ -11,12 +11,6 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "FiftyOne.settings")
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+# from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
-application = get_wsgi_application()
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-
-application = Cling(get_wsgi_application())
